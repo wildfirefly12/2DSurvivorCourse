e1e95532fbe45aec8c6a3bc01821363a8bcc7751
wildfirefly <wildfirefly12@gmail.com>
2023-09-18 07:46:55 -0600

    game pauses when upgrade screen pops up

--- scenes/auto_load/GameEvents.gd
+++ scenes/auto_load/GameEvents.gd
@@ -2,7 +2,12 @@ extends Node
 
 
 signal experience_orb_collected(number: float)
+signal ability_upgrade_added(upgrade: AbilityUpgrade, current_upgrades: Dictionary)
 
 
 func emit_experience_orb_collected(number: float):
 	experience_orb_collected.emit(number)
+
+
+func emit_ability_upgrade_added(upgrade: AbilityUpgrade, current_upgrades: Dictionary):
+	ability_upgrade_added.emit(upgrade, current_upgrades)
