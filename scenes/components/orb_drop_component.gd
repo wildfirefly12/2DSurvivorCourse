609ebb40bbb973c765db98f71dbbc2c648b7d79e
wildfirefly <wildfirefly12@gmail.com>
2023-09-18 21:36:13 -0600

    implemented player health

--- scenes/components/orb_drop_component.gd
+++ scenes/components/orb_drop_component.gd
@@ -20,5 +20,8 @@ func on_died():
 		return
 	var spawn_position = (owner as Node2D).global_position
 	var orb_instance = orb_scene.instantiate() as Node2D
-	owner.get_parent().add_child(orb_instance)
+	
+	var entities_layer = get_tree().get_first_node_in_group("entities_layer")
+	
+	entities_layer.add_child(orb_instance)
 	orb_instance.position = spawn_position
